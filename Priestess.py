d80e7df2a11bc7e2618c4362ca99916f94b8e291
jenniferboutell <57915509+jenniferboutell@users.noreply.github.com>
2022-02-20 14:55:06 -0800

    Priestess with constructor

--- Priestess.py
+++ Priestess.py
@@ -1,7 +1,20 @@
 from Hero import *
+from Healable import *
 
-class Priestess:
-    pass
 
-def special_skill:
-    pass
+class Priestess(Hero, Healable):
+    def __init__(self, game, name, hit_points, hit_points_max, attack_speed, attack_behavior, chance_to_hit,
+                 minimum_damage, maximum_damage, chance_to_block, special_skill, chance_to_heal):
+        super().__init__(self, game, name, hit_points, hit_points_max, attack_speed, attack_behavior, chance_to_hit,
+                         minimum_damage, maximum_damage, chance_to_block, special_skill, chance_to_heal)
+        self.__hit_points = 75
+        self.__attack_speed = 5
+        self.__chance_to_hit = .7
+        self.__minimum_damage = 25
+        self.__maximum_damage = 45
+        self.__chance_to_block = 0.3
+        self.__special_skill = 'Heal'
+        self.__chance_to_heal = 0.5
+        self.__minimum_heal_points = 30
+        self.__maximum_heal_points = 50
+        
