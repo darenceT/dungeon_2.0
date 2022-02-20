beb1b74b08919d30b9b5bf0fb0dc8c140f51f2f7
jenniferboutell <57915509+jenniferboutell@users.noreply.github.com>
2022-02-20 14:53:01 -0800

    Fleshing out Monster (not much to it)

--- Monster.py
+++ Monster.py
@@ -1,14 +1,23 @@
 from DungeonCharacter import *
-
-class Monster(ABC):
-    pass
+from Healable import *
 
 
+class Monster(ABC, DungeonCharacter):
+    def __init__(self, game, name, hit_points, hit_points_max, attack_speed, attack_behavior, chance_to_hit,
+                 minimum_damage, maximum_damage, chance_to_heal, minimum_heal_points, maximum_heal_points):
+        super().__init__(game, name, hit_points, hit_points_max, attack_speed, attack_behavior, chance_to_hit,
+                 minimum_damage, maximum_damage, chance_to_heal, minimum_heal_points, maximum_heal_points)
 
 
 
 
+from DungeonCharacter import *
+from Healable import *
 
 
-
+class Monster(ABC, DungeonCharacter):
+    def __init__(self, game, name, hit_points, hit_points_max, attack_speed, attack_behavior, chance_to_hit,
+                 minimum_damage, maximum_damage, chance_to_heal, minimum_heal_points, maximum_heal_points):
+        super().__init__(game, name, hit_points, hit_points_max, attack_speed, attack_behavior, chance_to_hit,
+                 minimum_damage, maximum_damage, chance_to_heal, minimum_heal_points, maximum_heal_points)
 
