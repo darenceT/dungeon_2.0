f58718f151c8083d5c152980f7d04887e8f33de3
jenniferboutell <57915509+jenniferboutell@users.noreply.github.com>
2022-03-01 18:58:31 -0800

    Fixes to monster

--- Monster.py
+++ Monster.py
@@ -2,22 +2,8 @@ from DungeonCharacter import *
 from Healable import *
 
 
-class Monster(ABC, DungeonCharacter):
-    def __init__(self, game, name, hit_points, hit_points_max, attack_speed, attack_behavior, chance_to_hit,
+class Monster(Healable, DungeonCharacter):
+    def __init__(self, game, name, hit_points, hit_points_max, attack_speed, chance_to_hit,
                  minimum_damage, maximum_damage, chance_to_heal, minimum_heal_points, maximum_heal_points):
-        super().__init__(game, name, hit_points, hit_points_max, attack_speed, attack_behavior, chance_to_hit,
+        super().__init__(game, name, hit_points, attack_speed, chance_to_hit,
                  minimum_damage, maximum_damage, chance_to_heal, minimum_heal_points, maximum_heal_points)
-
-
-
-
-from DungeonCharacter import *
-from Healable import *
-
-
-class Monster(ABC, DungeonCharacter):
-    def __init__(self, game, name, hit_points, hit_points_max, attack_speed, attack_behavior, chance_to_hit,
-                 minimum_damage, maximum_damage, chance_to_heal, minimum_heal_points, maximum_heal_points):
-        super().__init__(game, name, hit_points, hit_points_max, attack_speed, attack_behavior, chance_to_hit,
-                 minimum_damage, maximum_damage, chance_to_heal, minimum_heal_points, maximum_heal_points)
-
