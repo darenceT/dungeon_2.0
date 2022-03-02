dd7c1b24b6f7adcbb94a9ed38d528b5a8b4d9ae5
jenniferboutell <57915509+jenniferboutell@users.noreply.github.com>
2022-03-01 18:59:23 -0800

    Fixed super in monster classes

--- Gremlin.py
+++ Gremlin.py
@@ -1,16 +1,9 @@
 from Monster import *
+from Healable import *
 
 
 class Gremlin(Monster):
-    def __init__(self, game, name, hit_points, hit_points_max, attack_speed, attack_behavior, chance_to_hit,
-                 minimum_damage, maximum_damage, chance_to_heal, minimum_heal_points, maximum_heal_points):
-        super().__init__(game, name, hit_points, hit_points_max, attack_speed, attack_behavior, chance_to_hit,
+    def __init__(self, game, name, hit_points=70, attack_speed=5, chance_to_hit=0.8,
+                 minimum_damage=15, maximum_damage=30, chance_to_heal=0.4, minimum_heal_points=20, maximum_heal_points=40):
+        super().__init__(game, name, hit_points, attack_speed, chance_to_hit,
                          minimum_damage, maximum_damage, chance_to_heal, minimum_heal_points, maximum_heal_points)
-        self.__hit_points = 70
-        self.__attack_speed = 5
-        self.__chance_to_hit = 0.8
-        self.__minimum_damage = 15
-        self.__maximum_damage = 30
-        self.__chance_to_heal = 0.4
-        self.__minimum_heal_points = 20
-        self.__maximum_heal_points = 40
