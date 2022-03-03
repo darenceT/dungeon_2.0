883a270aa942fadba6cfc6fd36df49fcb96082db
jenniferboutell <57915509+jenniferboutell@users.noreply.github.com>
2022-03-02 17:49:52 -0800

    fixed healable relationship

--- Ogre.py
+++ Ogre.py
@@ -1,9 +1,9 @@
 from Monster import *
-from Healable import *
 
 
-class Gremlin(Monster):
-    def __init__(self, game, name, hit_points=70, attack_speed=5, chance_to_hit=0.8,
-                 minimum_damage=15, maximum_damage=30, chance_to_heal=0.4, minimum_heal_points=20, maximum_heal_points=40):
+class Ogre(Monster):
+    def __init__(self, game, name, hit_points=200, attack_speed=2, chance_to_hit=0.6,
+                 minimum_damage=30, maximum_damage=60, chance_to_heal=0.1, minimum_heal_points=30,
+                 maximum_heal_points=60):
         super().__init__(game, name, hit_points, attack_speed, chance_to_hit,
                          minimum_damage, maximum_damage, chance_to_heal, minimum_heal_points, maximum_heal_points)
