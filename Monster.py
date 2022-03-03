92aa629dbfa0d03634087f9a101ec7f72942a1ba
jenniferboutell <57915509+jenniferboutell@users.noreply.github.com>
2022-03-02 17:48:52 -0800

    Minor fix to monster/healable

--- Monster.py
+++ Monster.py
@@ -3,7 +3,7 @@ from Healable import *
 
 
 class Monster(Healable, DungeonCharacter):
-    def __init__(self, game, name, hit_points, hit_points_max, attack_speed, chance_to_hit,
+    def __init__(self, game, name, hit_points, attack_speed, chance_to_hit,
                  minimum_damage, maximum_damage, chance_to_heal, minimum_heal_points, maximum_heal_points):
         super().__init__(game, name, hit_points, attack_speed, chance_to_hit,
                  minimum_damage, maximum_damage, chance_to_heal, minimum_heal_points, maximum_heal_points)
