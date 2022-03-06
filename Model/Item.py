967103ce6a610c5c9dd1839ea12ab2c7cd79726a
JP Montagnet <x-github@jope.net>
2022-03-05 22:56:03 -0800

    Nearly complete implementation of Items hierarchy, including Pillars.

--- Model/Item.py
+++ Model/Item.py
@@ -1,27 +1,139 @@
-class Item:
-    __fixed: bool = False
+from abc import ABCMeta
+from collections import namedtuple
+from typing import Any
 
-    def __init__(self):
-        self.__owner = None
+
+class Item(metaclass=ABCMeta):
+    __portable: bool = True
+
+    def __init__(self, owner: Any = None):
+        self.__owner = owner
+
+    @property
+    def owner(self):
+        return self.__owner
+
+    @owner.setter
+    def owner(self, val: Any):
+        self.__owner = val
+
+    @property
+    def portable(self):
+        return self.__portable
 
 
 class Pit(Item):
-    __fixed = True
+    __portable = False
+
+    __damage: int = 10
+
+    def __init__(self):
+        super().__init__()
+
+    @property
+    def damage(self):
+        return self.__damage
 
 
 class Bomb(Item):
-    pass
+    __damage: int = 10
+
+    def __init__(self):
+        super().__init__()
+
+    # TODO ...?
 
 
-class Potion(Item):
-    pass
+class Potion(Item, metaclass=ABCMeta):
+    """ Abstract base class for potions.
+     At present, nothing to distinguish it from Item base class,
+     except as a logical superclass of only the Potion classes.
+     """
+    def __init__(self):
+        super().__init__()
 
 
 class VisionPotion(Potion):
-    pass
+    radius: int = 1
+
+    def __init__(self):
+        super().__init__()
 
 
 class HealthPotion(Potion):
-    pass
+    points: int = 10
+
+    def __init__(self):
+        super().__init__()
+
+
+class Pillar(Item):
+    """ The physical embodiment of one of the Pillars of Object-Oriented design.
+    Sort of like a Badge of Participation. Except it is not portable.
+    Perhaps more a Monument of Objectification? In any case...
+    there are exactly four instances, each unique.
+     """
+    __portable = False
+
+    __tmpl = namedtuple('__template', 'abbr name')
+    __templates: list[__tmpl] = [
+        __tmpl('A', 'Abstraction'),
+        __tmpl('E', 'Encapsulation'),
+        __tmpl('I', 'Inheritance'),
+        __tmpl('P', 'Polymorphism'),
+    ]
+    __instances = []
+
+    def __init__(self, name: str):
+        super().__init__()
+        for inst in Pillar.__instances:
+            if name == inst.name:
+                raise ValueError(f"Pillar '{name}' already instantiated")
+        tmpl: Pillar.__tmpl
+        for tmpl in Pillar.__templates:
+            if name in tmpl:
+                self.__abbr: str = tmpl.abbr
+                self.__name: str = name
+                Pillar.__instances.append(self)
+                return
+        raise ValueError(f"Pillar '{name}' is not valid")
+
+    @property
+    def abbr(self):
+        return self.__abbr
+
+    @property
+    def name(self):
+        return self.__name
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.name})"
+
+
+Abstraction = Pillar('Abstraction')
+Encapsulation = Pillar('Encapsulation')
+Inheritance = Pillar('Inheritance')
+Polymorphism = Pillar('Polymorphism')
+Pillars = (Abstraction, Encapsulation, Inheritance, Polymorphism)
+
+
+if __name__ == '__main__':
+
+    def example():
+        # TODO pit
+        # TODO bomb
+        # TODO potions
+        print("The Pillars of OO...")
+        for p in Pillars:
+            print(p)
+        for name in ('Obfuscation', 'Abstraction'):
+            print(f"Try to create pillar '{name}'...")
+            try:
+                p = Pillar(name)
+                print(p)
+            except ValueError as e:
+                print(f'Nope! {e}')
+
+    example()
 
 # END
--- Model/Room.py
+++ Model/Room.py
@@ -1,13 +1,36 @@
+from typing import Optional
+
 from Item import *
 from Cell import Cell
+from Util import obj_repr
 
 
 class RoomContents:
     def __init__(self):
+        self.__ingress: bool = False
+        self.__egress: bool = False
+        # TODO instances, not ints/bools
         self.__pit: bool = False
         self.__health: int = 0
         self.__vision: int = 0
         self.__bomb: int = 0
+        self.__pillar: Optional[str] = None
+
+    @property
+    def ingress(self):
+        return self.__ingress
+
+    @ingress.setter
+    def ingress(self, val: bool):
+        self.__ingress = val
+
+    @property
+    def egress(self):
+        return self.__egress
+
+    @egress.setter
+    def egress(self, val: bool):
+        self.__egress = val
 
     @property
     def pit(self):
@@ -41,31 +64,93 @@ class RoomContents:
     def bomb(self, val: int):
         self.__bomb = val
 
-    def add(self, *item_types):
-        for i in item_types:
-            if not issubclass(i, Item):
-                raise TypeError(f'room {self} cannot add non-item {i}')
-            if issubclass(i, Pit):
-                self.pit = True
-            elif issubclass(i, HealthPotion):
-                self.health += 1
-            elif issubclass(i, VisionPotion):
-                self.vision += 1
-            elif issubclass(i, Bomb):
-                self.bomb += 1
+    @property
+    def pillar(self):
+        return self.__pillar
 
-    def has(self, item_type):
-        i = item_type
-        if not issubclass(i, Item):
-            raise TypeError(f'room {self} cannot haz non-item {i}')
-        if issubclass(i, Pit):
-            return bool(self.pit)
-        elif issubclass(i, HealthPotion):
-            return self.health > 0
-        elif issubclass(i, VisionPotion):
-            return self.vision > 0
-        elif issubclass(i, Bomb):
-            return self.bomb > 0
+    @pillar.setter
+    def pillar(self, val: Optional[Pillar]):
+        self.__pillar = val
+
+    def can_add(self, obj: Any):
+        if self.ingress or self.egress:
+            return False
+        else:
+            if isinstance(obj, Pillar):
+                # print(f"{obj} is Pillar instance")
+                return True
+            elif isinstance(obj, Item):
+                # print(f"{obj} is Item instance")
+                return True
+            elif isinstance(obj, type) and issubclass(obj, Pillar):
+                # print(f"{obj} is Pillar subclass")
+                return True
+            elif isinstance(obj, type) and issubclass(obj, Item):
+                # print(f"{obj} is Item subclass")
+                return True
+        return False
+
+    def can_have(self, obj: Any):
+        if self.ingress or self.egress:
+            return False
+        else:
+            if isinstance(obj, type):
+                if issubclass(obj, Item):
+                    return True
+                # if issubclass(obj, Pillar):
+                #     return True
+            elif isinstance(obj, Item):
+                return True
+            # elif isinstance(obj, Pillar):
+            #     return True
+        return False
+
+    def add(self, *items) -> None:
+        for i in items:
+            if not self.can_add(i):
+                # TODO raising exception mauybe too extreme, but at least log somewhere
+                raise TypeError(f"room cannot add {i}")
+                # continue
+            # TODO instantiate (if passed class) and add
+            if isinstance(i, type):
+                if issubclass(i, Pit):
+                    self.pit = True
+                elif issubclass(i, HealthPotion):
+                    self.health += 1
+                elif issubclass(i, VisionPotion):
+                    self.vision += 1
+                elif issubclass(i, Bomb):
+                    self.bomb += 1
+            elif isinstance(i, Pillar):
+                self.pillar = i
+
+    def has(self, item: Any) -> bool:
+        i = item
+        if not self.can_have(i):
+            # TODO raising exception mauybe too extreme, but at least log somewhere
+            raise TypeError(f"room cannot have {i}")
+            # return False
+        if isinstance(i, type):
+            if issubclass(i, Pit):
+                return bool(self.pit)
+            elif issubclass(i, VisionPotion):
+                return self.vision > 0
+            elif issubclass(i, HealthPotion):
+                return self.health > 0
+            elif i is Potion:
+                return self.has(HealthPotion) or self.has(VisionPotion)
+            elif issubclass(i, VisionPotion):
+                return self.vision > 0
+            elif issubclass(i, Bomb):
+                return self.bomb > 0
+            elif issubclass(i, Pillar):
+                return bool(self.pillar)
+        elif isinstance(i, Pillar):
+            return i is self.pillar
+        return False
+
+    def __repr__(self):
+        return obj_repr(self)
 
 
 class Room(Cell):
@@ -85,33 +170,44 @@ class Room(Cell):
         self.__contents = val
 
 
-def example():
-    """Example code"""
-    from pprint import PrettyPrinter
-
-    pp = PrettyPrinter(compact=True)
-
-    print(f'Create 2 RoomContents...')
-    cont1 = RoomContents()
-    cont1.add(HealthPotion)
-    cont1.add(Bomb)
-    print(f'Contents 1: {pp.pformat(vars(cont1))}')
-
-    cont2 = RoomContents()
-    cont2.add(Pit, VisionPotion)
-    print(f'Contents 2: {pp.pformat(vars(cont2))}')
-
-    print(f'Create 2 Rooms...')
-    room1 = Room()
-    room1.contents.add(Pit)
-    print(f'Room 1: {pp.pformat(vars(room1))}')
-
-    room2 = Room()
-    room2.contents.add(VisionPotion, HealthPotion)
-    print(f'Room 2: {pp.pformat(vars(room2))}')
-
-
 if __name__ == '__main__':
+
+    def example():
+        """Example code"""
+        print(f'RoomContents 1...')
+        cont = RoomContents()
+        cont.add(HealthPotion)
+        cont.add(Bomb)
+        print(cont)
+        print(f'has a Potion? {cont.has(Potion)}')
+        print(f'has a Vision Potion? {cont.has(VisionPotion)}')
+        print(f'has a Health Potion? {cont.has(HealthPotion)}')
+        print(f'has a Bomb? {cont.has(Bomb)}')
+
+        print(f'RoomContents 2...')
+        cont = RoomContents()
+        cont.add(Pit, VisionPotion)
+        cont.add(Abstraction)
+        print(cont)
+        print(f'has a Pit? {cont.has(Pit)}')
+        print(f'has a Potion? {cont.has(Potion)}')
+        print(f'has a Vision Potion? {cont.has(VisionPotion)}')
+        print(f'has a Health Potion? {cont.has(HealthPotion)}')
+        print(f'has a Bomb? {cont.has(Bomb)}')
+        print(f'has a Pillar? {cont.has(Pillar)}')
+        print(f'has Abstraction? {cont.has(Abstraction)}')
+        print(f'has Encapsulation? {cont.has(Encapsulation)}')
+
+        print(f'Room 1...')
+        room = Room()
+        room.contents.add(Pit)
+        print(room)
+
+        print(f'Room 2...')
+        room = Room()
+        room.contents.add(VisionPotion, HealthPotion)
+        print(room)
+
     example()
 
 # END
